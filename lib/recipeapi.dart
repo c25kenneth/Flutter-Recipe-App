dc1163f98115e181e6f8d5230959e3547980e1ba
thequickbrownfoxjumpedoverthelazydog <70728301+thequickbrownfoxjumpedoverthelazydog@users.noreply.github.com>
2022-05-15 08:42:02 -0700

    Update recipeapi.dart

--- lib/recipeapi.dart
+++ lib/recipeapi.dart
@@ -8,7 +8,7 @@ class RecipeApi{
     var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list', {"limit": "15", "start": "0", "tag": "list.recipe.popular"});
 
     final response = await http.get(uri, headers: {
-      'X-RapidAPI-Key': 'c7206de6e0mshcfffe32d8293449p1e04bdjsn8e67d8b7b5e4',
+      'X-RapidAPI-Key': '**********************',
       'X-RapidAPI-Host': 'yummly2.p.rapidapi.com',
       "useQueryString" : "true"
     }); 
